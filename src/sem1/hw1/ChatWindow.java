4243982fff687ac8a2c36aeb7e1ed4fdcd7e8061
Nikos-I <nik.norht@gmail.com>
2023-10-09 14:58:43 +0300

    seminar1 finish

--- src/sem1/hw1/ChatWindow.java
+++ src/sem1/hw1/ChatWindow.java
@@ -5,15 +5,16 @@ import java.awt.event.*;
 import java.io.IOException;
 import java.util.List;
 
+@SuppressWarnings("ALL")
 public class ChatWindow extends JDialog {
 
-    public static String server="";
-    public static String port="";
-    public static String login="";
-    public static String password="";
+    public static String server = "";
+    public static String port = "";
+    public static String login = "";
+    public static String password = "";
 
-    public final String FILENAMELOG="./chat.log";
-    private LogHandler log;
+    public final String FILENAMELOG = "./chat.log";
+    private final LogHandler log;
 
     private JPanel pnlMain1;
     private JButton btnSend;
@@ -21,7 +22,6 @@ public class ChatWindow extends JDialog {
     private JButton btnConnect;
     private JButton btnSave;
     private JTextArea areaChat;
-    private JTextArea areaMessage;
     private JToolBar tbMain;
     private JPanel pnlServer;
     private JPanel pnlLog;
@@ -32,29 +32,24 @@ public class ChatWindow extends JDialog {
     private JPanel pnlChat;
     private JButton btnExit;
     private JScrollPane scrollArea;
+    private JTextField txtMessage;
 
     public ChatWindow() throws IOException {
-        log=new LogHandler(FILENAMELOG);
+        log = new LogHandler(FILENAMELOG);
 
         setContentPane(pnlMain1);
         setModal(true);
         getRootPane().setDefaultButton(btnSend);
 
-        btnSend.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    onSend();
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
+        btnSend.addActionListener(e -> {
+            try {
+                onSend();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
             }
         });
 
-        btnCancel.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        });
+        btnCancel.addActionListener(e -> onCancel());
 
         // call onCancel() when cross is clicked
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
@@ -65,90 +60,77 @@ public class ChatWindow extends JDialog {
         });
 
         // call onCancel() on ESCAPE
-        pnlMain1.registerKeyboardAction(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        pnlMain1.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 
-        btnConnect.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    onConnect();
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
+        btnConnect.addActionListener(e -> {
+            try {
+                onConnect();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
             }
         });
 
-        btnDisconnect.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    onDisConnect();
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
+        btnDisconnect.addActionListener(e -> {
+            try {
+                onDisConnect();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
             }
         });
-        btnSave.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    log.write(areaChat.getText());
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
+        btnSave.addActionListener(e -> {
+            try {
+                log.write(areaChat.getText());
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
 
-            }
         });
-        btnLoad.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                onload();
-            }
-        });
-        btnExit.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
+        btnLoad.addActionListener(e -> onload());
+        btnExit.addActionListener(e -> System.exit(0));
+
+        setLocationRelativeTo(null);
+
     }
 
     private void onSend() throws IOException {
-        String msgText=String.format("%s: %s\n", login, areaMessage.getText());
+        String msgText = String.format("%s: %s\n", login, txtMessage.getText());
         areaChat.append(msgText);
         log.write(msgText);
     }
 
     private void onCancel() {
-        areaMessage.setText("");
+        txtMessage.setText("");
     }
 
     private void onConnect() throws IOException {
-        Connect dialog=new Connect();
+        Connect dialog = new Connect();
         dialog.pack();
         dialog.setVisible(true);
         areaChat.setText("");
         onload();
-        String msgText=String.format("Пользователь %s вошел в чат\n", login);
+        String msgText = String.format("Пользователь %s вошел в чат\n", login);
 //        pnlMessage.setToolTipText(String.format("Сообщение от %s", login));
         areaChat.append(msgText);
         log.write(msgText);
+        txtMessage.setEnabled(true);
+        btnSend.setEnabled(true);
+        btnCancel.setEnabled(true);
     }
 
     private void onDisConnect() throws IOException {
-        String msgText=String.format("Пользователь %s покинул чат\n", login);
+        String msgText = String.format("Пользователь %s покинул чат\n", login);
         areaChat.append(msgText);
         log.write(msgText);
+        txtMessage.setEnabled(false);
+        btnSend.setEnabled(false);
+        btnCancel.setEnabled(false);
+
     }
 
     private void onload() {
         List<String> logString;
         try {
-            logString=log.read();
+            logString = log.read();
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
--- src/sem1/hw1/Connect.java
+++ src/sem1/hw1/Connect.java
@@ -1,7 +1,6 @@
 package sem1.hw1;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.*;
 
 public class Connect extends JDialog {
@@ -20,17 +19,9 @@ public class Connect extends JDialog {
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
 
-        buttonOK.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onOK();
-            }
-        });
+        buttonOK.addActionListener(e -> onOK());
 
-        buttonCancel.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        });
+        buttonCancel.addActionListener(e -> onCancel());
 
         // call onCancel() when cross is clicked
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
@@ -41,11 +32,8 @@ public class Connect extends JDialog {
         });
 
         // call onCancel() on ESCAPE
-        contentPane.registerKeyboardAction(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        setLocationRelativeTo(null);
     }
 
     private void onOK() {
